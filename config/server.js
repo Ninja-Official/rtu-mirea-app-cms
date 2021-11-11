a03113589cbfb2fa75cbcfaff25f045aac35c7cb
Oniel <51058739+0niel@users.noreply.github.com>
2021-11-11 14:34:52 +0300

    Upload project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7ca30d91c5ba560e9e5b17e8b055421'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '248734f6-0db3-440d-a0c8-2447d2f19fa1'
+};
