5597f9041190b9a3ac7e54437476a5d3701d6d94
Oniel <51058739+0niel@users.noreply.github.com>
2022-01-09 14:14:56 +0300

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,4 +8,18 @@ module.exports = [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'mirea-strapi.s3.eu-west-2.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'mirea-strapi.s3.eu-west-2.amazonaws.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
