81e39f27cae3fbe87735d7b07a0e6b60e09cdcc1
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-01-20 20:00:40 +0300

    Formatting code

--- src/api/search-students-bot-person/controllers/search-students-bot-person.js
+++ src/api/search-students-bot-person/controllers/search-students-bot-person.js
@@ -1,38 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  * search-students-bot-person controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const typeEnum = Object.freeze({ request: 'request', query: 'query' });
+const typeEnum = Object.freeze({ request: "request", query: "query" });
 
-module.exports = createCoreController('api::search-students-bot-person.search-students-bot-person', ({ strapi }) => ({
+module.exports = createCoreController(
+  "api::search-students-bot-person.search-students-bot-person",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { query, userId, type } = ctx.request.query;
+      const count = 30;
 
-  async find(ctx) {
-    const { query, userId, type } = ctx.request.query;
-    const count = 30;
+      if (!query) {
+        return ctx.badRequest("Query is required");
+      }
+      if (query.length < 3) {
+        return ctx.badRequest("Query must be at least 3 characters long");
+      }
+      if (userId === undefined) {
+        return ctx.badRequest("User ID is required");
+      }
+      if (type === undefined) {
+        return ctx.badRequest("Type is required");
+      }
 
-    if (!query) {
-      return ctx.badRequest('Query is required');
-    }
-    if (query.length < 3) {
-      return ctx.badRequest('Query must be at least 3 characters long');
-    }
-    if (userId === undefined) {
-      return ctx.badRequest('User ID is required');
-    }
-    if (type === undefined) {
-      return ctx.badRequest('Type is required');
-    }
+      let meta = { count };
+      const lowerCaseQuery = query.toLowerCase();
 
-    let meta = { count };
-    const lowerCaseQuery = query.toLowerCase();
-
-
-    const data = await strapi.db.connection.context.raw(
-      `SELECT limited_people.* FROM (
+      const data = await strapi.db.connection.context
+        .raw(
+          `SELECT limited_people.* FROM (
         SELECT id, sdo_id AS "sdoId", last_name AS "lastName", first_name AS "firstName",
           middle_name AS "middleName", email, "group",
           code, leaderid_id AS "leaderidId", phone,
@@ -51,15 +52,19 @@ module.exports = createCoreController('api::search-students-bot-person.search-st
         ) AS people
         LIMIT :count) as limited_people
       ORDER BY "lastName", "firstName", "middleName", "group"`,
-      { query : lowerCaseQuery, count : count }
-    ).then((res) => {return res.rows.map((row) => {
-      const { id, ...attributes } = row;
-      return { id, attributes };
-    })});
-    meta.count = data.length;
+          { query: lowerCaseQuery, count: count }
+        )
+        .then((res) => {
+          return res.rows.map((row) => {
+            const { id, ...attributes } = row;
+            return { id, attributes };
+          });
+        });
+      meta.count = data.length;
 
-    meta.total = await strapi.db.connection.context.raw(
-      `SELECT COUNT(people.*) FROM (
+      meta.total = await strapi.db.connection.context
+        .raw(
+          `SELECT COUNT(people.*) FROM (
           (SELECT * FROM search_students_bot_people
             WHERE (
             LOWER(CONCAT(last_name, ' ', first_name, ' ', middle_name)) ~ :query
@@ -70,75 +75,97 @@ module.exports = createCoreController('api::search-students-bot-person.search-st
           (SELECT * FROM search_students_bot_people
             WHERE LOWER("group") ~ :query AND (published_at IS NOT NULL))
       ) AS people`,
-      { query : lowerCaseQuery }
-    ).then((res) => {return parseInt(res.rows[0].count)})
+          { query: lowerCaseQuery }
+        )
+        .then((res) => {
+          return parseInt(res.rows[0].count);
+        });
 
-    const permission = await strapi.db.query('api::search-students-bot-extended-access-permission.search-students-bot-extended-access-permission')
-      .findOne(
-        {
-          select : ['id', 'requestExtendedAccess', 'queryExtendedAccess'],
-          where: { $and: [{ telegramUserId: userId }, { published_at : { $not : null } }] }
-        },
-      )
+      const permission = await strapi.db
+        .query(
+          "api::search-students-bot-extended-access-permission.search-students-bot-extended-access-permission"
+        )
+        .findOne({
+          select: ["id", "requestExtendedAccess", "queryExtendedAccess"],
+          where: {
+            $and: [
+              { telegramUserId: userId },
+              { published_at: { $not: null } },
+            ],
+          },
+        });
 
-    if (!permission ||
+      if (
+        !permission ||
         (!permission.requestExtendedAccess && type === typeEnum.request) ||
         (!permission.queryExtendedAccess && type === typeEnum.query)
-    ) {
-      const allowedFields = [
-        'lastName', 'firstName', 'middleName', 'code',
-        'group', 'birthday', 'vkId', 'email', 'jobTitle'
-      ];
+      ) {
+        const allowedFields = [
+          "lastName",
+          "firstName",
+          "middleName",
+          "code",
+          "group",
+          "birthday",
+          "vkId",
+          "email",
+          "jobTitle",
+        ];
 
-      // filter each item in data array by allowedFields
-      const filteredData = data.map(item => {
-        const filteredItem = { id : item.id, attributes: {} };
-        allowedFields.forEach(field => {
-          filteredItem.attributes[field] = item.attributes[field];
+        // filter each item in data array by allowedFields
+        const filteredData = data.map((item) => {
+          const filteredItem = { id: item.id, attributes: {} };
+          allowedFields.forEach((field) => {
+            filteredItem.attributes[field] = item.attributes[field];
+          });
+          return filteredItem;
         });
-        return filteredItem;
-      });
-      return { data: filteredData, meta };
-    }
+        return { data: filteredData, meta };
+      }
 
-    return { data, meta };
-  },
+      return { data, meta };
+    },
 
-  async getFullTeacherName(ctx) {
-    const { rawNames } = ctx.request.query;
+    async getFullTeacherName(ctx) {
+      const { rawNames } = ctx.request.query;
 
-    if (!rawNames) {
-      return ctx.badRequest('Raw names are required');
-    }
-    const names = matchAll('[ЁёА-я]+\\ [ЁёА-я]\\.[ЁёА-я]\\.', rawNames).slice(0, 5);
+      if (!rawNames) {
+        return ctx.badRequest("Raw names are required");
+      }
+      const names = matchAll("[ЁёА-я]+\\ [ЁёА-я]\\.[ЁёА-я]\\.", rawNames).slice(
+        0,
+        5
+      );
 
-    let result = [];
-    for (const name of names) {
-      const data = await strapi.db.connection.context.raw(
-        `SELECT DISTINCT ON ("lastName", "firstName", "middleName") last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName"
+      let result = [];
+      for (const name of names) {
+        const data = await strapi.db.connection.context
+          .raw(
+            `SELECT DISTINCT ON ("lastName", "firstName", "middleName") last_name AS "lastName", first_name AS "firstName", middle_name AS "middleName"
           FROM search_students_bot_people
           WHERE email LIKE '%@mirea.ru'
              AND CONCAT(last_name, ' ', SUBSTR(first_name, 1, 1), '.', SUBSTR(middle_name, 1, 1), '.') = :name
              AND published_at IS NOT NULL
           LIMIT 10`,
-        { name }
-      ).then((res) => {
-        return res.rows;
-      });
-      result.push({ rawName: name, possibleFullNames: data });
-    }
-    return result;
-  },
+            { name }
+          )
+          .then((res) => {
+            return res.rows;
+          });
+        result.push({ rawName: name, possibleFullNames: data });
+      }
+      return result;
+    },
+  })
+);
 
-}));
-
-function matchAll(pattern,haystack){
-  var regex = new RegExp(pattern,"g")
+function matchAll(pattern, haystack) {
+  var regex = new RegExp(pattern, "g");
   var matches = [];
 
   var match_result = haystack.match(regex);
 
-  for (let index in match_result){
+  for (let index in match_result) {
     var item = match_result[index];
     matches[index] = item.match(regex)[0];
   }
