44f092ec791bec9f9139e3d4e17765d6b554c8cd
Валера <70258211+ValeryVerkhoturov@users.noreply.github.com>
2023-01-26 18:04:04 +0300

    Add landing collection types (#6)
    * Add landing collection types
    
    * Add authors to project card
    
    * Add order field for landing components

---
+++ src/api/landing-project-group/controllers/landing-project-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * landing-project-group controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::landing-project-group.landing-project-group');
