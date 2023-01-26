09e73b585f219da630e0a80fd0ad69fa643e77f8
Valery Verkhoturov <verkhoturov.valery@icloud.com>
2023-01-26 13:47:02 +0300

    Add landing collection types

---
+++ src/api/landing-project-group/routes/landing-project-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * landing-project-group router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::landing-project-group.landing-project-group');
---
+++ src/api/landing-project-group/services/landing-project-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * landing-project-group service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::landing-project-group.landing-project-group');
