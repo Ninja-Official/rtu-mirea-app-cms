4ae6f7f4038de9d9ed7eb5c8c3c23d65083a4252
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-01-30 16:05:29 +0300

    Add proxy settings to `config\server.js`

--- config/server.js
+++ config/server.js
@@ -3,6 +3,8 @@ const cronTasks = require("@webbio/strapi-plugin-scheduler/cron-task");
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "https://cms.mirea.ninja"),
+  proxy: true,
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
