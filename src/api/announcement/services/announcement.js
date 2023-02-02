f79fa5dd9bca682dbdcf408f6609d3a81f7b4396
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-02-02 13:39:03 +0300

    Add auto-population all relations in news service

--- src/api/announcement/services/announcement.js
+++ src/api/announcement/services/announcement.js
@@ -1,9 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * announcement service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::announcement.announcement');
+module.exports = createCoreService(
+  "api::announcement.announcement",
+  ({ strapi }) => ({
+    async find(...args) {
+      // Populate all relations
+      const newArgs = [
+        {
+          fields: ["isImportant", "title", "text", "date"],
+          populate: { tags: true, images: true },
+        },
+        ...args,
+      ];
+
+      const { results, pagination } = await super.find(...newArgs);
+
+      return { results, pagination };
+    },
+  })
+);
