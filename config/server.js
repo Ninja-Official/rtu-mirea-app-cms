f7bc892477fbc8a98da7a81fce98b753b2c80a3e
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-05-22 16:19:07 +0300

    Upgrade Strapi to v4.10.5 and add proxy env var

--- config/server.js
+++ config/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   url: env("PUBLIC_URL", "https://cms.mirea.ninja"),
-  proxy: true,
+  proxy: env.bool("IS_PROXIED", false),
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
   },
