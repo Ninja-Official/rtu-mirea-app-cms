40c46af2f08961acf6717cffe1d01aeafa689a18
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-05-22 18:18:36 +0300

    Create Employees collection and MapRooms

---
+++ src/api/map-room/controllers/map-room.ts
@@ -0,0 +1,7 @@
+/**
+ * map-room controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::map-room.map-room');
---
+++ src/api/map-room/services/map-room.ts
@@ -0,0 +1,7 @@
+/**
+ * map-room service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::map-room.map-room');
